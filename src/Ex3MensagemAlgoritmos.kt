3daee762a0dcf54229be738e3f65e100e54ae8eb
jennysival <jeniffersival@gmail.com>
2022-04-17 16:14:13 -0300

    Resolução do exercício 3

--- src/Ex3MensagemAlgoritmos.kt
+++ src/Ex3MensagemAlgoritmos.kt
@@ -1,3 +1,12 @@
 //Escreva um algoritmo que exiba 20 vezes a mensagem “Eu gosto de
 //estudar Algoritmos!”. Utilize o comando de repetição Enquanto
 //(While)
+
+fun main() {
+    var i = 1
+
+    while(i <= 20){
+        println("$i: Eu gosto de estudar algoritmos")
+        i++
+    }
+}

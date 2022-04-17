3c0239425822ca4a9717826bb9db73bbde9d20ee
jennysival <jeniffersival@gmail.com>
2022-04-17 18:11:50 -0300

    Exercício fibonacci incompleto

--- src/Ex8Fibonacci.kt
+++ src/Ex8Fibonacci.kt
@@ -3,3 +3,9 @@
 //elementos da série de Fibonacci são 0 e 1 e cada próximo termo é a soma
 //dos 2 anteriores a ele. No final imprima a mensagem "Fib(N) = X", onde X é
 //o N-ésimo termo da série de Fibonacci.
+
+fun main() {
+    println("Digite um número:")
+    val numeroDigitado = readln().toInt()
+
+}

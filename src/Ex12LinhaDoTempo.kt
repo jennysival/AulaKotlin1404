9da7188bcf6e2638b2689284c2b494e50bdb232f
jennysival <jeniffersival@gmail.com>
2022-04-17 19:20:41 -0300

    Resolução do exercício 12

--- src/Ex12LinhaDoTempo.kt
+++ src/Ex12LinhaDoTempo.kt
@@ -1,3 +1,20 @@
 //Escreva um algoritmo que leia o ano de nascimento de uma pessoa
 //e apresente uma linha do tempo, mostrando cada ano de vida da
 //pessoa e quantos anos ela tinha em cada ano até o ano atual.
+fun main() {
+
+    println("Digite seu ano de nascimento:")
+    val anoNascimento = readln().toInt()
+
+    val idade = 2022-anoNascimento
+
+    val vetorLinhaDoTempo = IntArray(idade)
+
+    for(i: Int in vetorLinhaDoTempo.indices){
+        vetorLinhaDoTempo[i] = anoNascimento+i
+    }
+
+    for(i: Int in vetorLinhaDoTempo.indices){
+        println("${vetorLinhaDoTempo[i]}: $i anos")
+    }
+}

308000249a31e2e3a03e8883b7f6d50bab058147
jennysival <jeniffersival@gmail.com>
2022-04-17 15:46:08 -0300

    resolução do primeiro exercício

--- src/Ex1Vetor5Valores.kt
+++ src/Ex1Vetor5Valores.kt
@@ -1,2 +1,15 @@
 //Faça um programa que leia 5 valores inteiros, armazene-os em um
 //vetor, calcule e apresente a soma destes valores.
+
+fun main() {
+    var vetorNumeros = IntArray(5)
+    var soma = 0
+
+    for(i: Int in vetorNumeros) {
+        print("Digite um número: ")
+        vetorNumeros[i] = readln().toInt()
+        soma = soma+vetorNumeros[i]
+    }
+
+    println("O resultado da soma dos números digitados é: $soma")
+}
--- src/Ex2DiaDaSemana.kt
+++ src/Ex2DiaDaSemana.kt
@@ -1,3 +1,6 @@
 //Escreva um algoritmo que leia um número inteiro (de 1 a 7)
 //representando o dia da semana e informe o nome do dia
 //correspondente.
+fun main() {
+
+}

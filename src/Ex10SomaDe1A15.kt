067cdf69a805bb58a7619d1a7ceb7c4b578a45b4
jennysival <jeniffersival@gmail.com>
2022-04-17 18:28:24 -0300

    Resolução do exercício 10

--- src/Ex10SomaDe1A15.kt
+++ src/Ex10SomaDe1A15.kt
@@ -1,2 +1,15 @@
 //Escreva um algoritmo que calcule a soma dos números de 1 a 15.
 //Utilize o comando de repetição Do While
+fun main() {
+
+    var soma = 0
+    var i = 0
+
+    do{
+        soma = soma+i
+        i++
+    }
+        while (i<=15)
+
+        println(soma)
+}

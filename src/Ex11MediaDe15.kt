aee02a0280bb32eb652e80c196b24ca456b6a61c
jennysival <jeniffersival@gmail.com>
2022-04-17 18:37:27 -0300

    Resolução do exercício 11

--- src/Ex11MediaDe15.kt
+++ src/Ex11MediaDe15.kt
@@ -1,2 +1,19 @@
 //Escreva um algoritmo que leia 15 números do usuário. Ao final exiba
 //a média dos 15 números. Utilize o comando de repetição Para (For)
+fun main() {
+    val numerosDoUsuario = IntArray(15)
+
+    for(i in 0..14){
+        println("Digite o ${i+1}º número:")
+        numerosDoUsuario[i] = readln().toInt()
+    }
+
+    var soma=0
+    for(i in 0..14){
+        soma = soma+numerosDoUsuario[i]
+    }
+
+    val media = soma/15
+
+    println("A média dos números digitados é: $media")
+}

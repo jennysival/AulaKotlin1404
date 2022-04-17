3e969c9a648da40450f60413931bb8baa91bda73
jennysival <jeniffersival@gmail.com>
2022-04-17 16:31:55 -0300

    Resolução do exercício 4

--- src/Ex4NumerosPares.kt
+++ src/Ex4NumerosPares.kt
@@ -1,3 +1,18 @@
 //Faça um programa que receba 10 números inteiros informados pela
 //pessoa usuária, armazene-os e verifique quais números são pares, e
 //exiba para o usuário apenas os números pares da lista.
+
+fun main() {
+    var vetorNumeros = IntArray(10)
+
+    for(i: Int in vetorNumeros.indices){
+        println("Digite um número inteiro:")
+        vetorNumeros[i] = readln().toInt()
+    }
+
+    for(i: Int in vetorNumeros.indices){
+        if(vetorNumeros[i] %2==0){
+            println("Os números pares são: ${vetorNumeros[i]}")
+        }
+    }
+}

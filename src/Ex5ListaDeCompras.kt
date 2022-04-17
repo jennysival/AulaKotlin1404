6ff24abefffb6346e4608fb5c89d5fd054b4e16d
jennysival <jeniffersival@gmail.com>
2022-04-17 16:48:11 -0300

    resolução exercício 5

--- src/Ex5ListaDeCompras.kt
+++ src/Ex5ListaDeCompras.kt
@@ -3,3 +3,21 @@
 //na lista de compras e após inserir a quantidade o programa deverá
 //permitir que o usuário insira os produtos nessa lista. Exiba a lista
 //completa para o usuário.
+
+fun main() {
+    println("Quantos itens terá na sua lista de compras?")
+    val qtdItens = readln().toInt()
+
+    var vetorListaDeCompras = arrayOfNulls<String>(qtdItens)
+
+    for(i: Int in vetorListaDeCompras.indices){
+        println("Digite o ${i+1}º item da sua lista:")
+        vetorListaDeCompras[i] = readln()
+    }
+
+    println("Aqui está sua lista de compras")
+
+    for(i: Int in vetorListaDeCompras.indices){
+        println("${i+1}: ${vetorListaDeCompras[i]}")
+    }
+}

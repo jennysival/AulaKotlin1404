dd29bbc861998370a1b8e2659f3e9aba0b07fd5d
jennysival <jeniffersival@gmail.com>
2022-04-18 14:27:54 -0300

    Resolução do fibonacci com base no java que fiz anteriormente

--- src/Ex8Fibonacci.kt
+++ src/Ex8Fibonacci.kt
@@ -3,36 +3,16 @@
 //elementos da série de Fibonacci são 0 e 1 e cada próximo termo é a soma
 //dos 2 anteriores a ele. No final imprima a mensagem "Fib(N) = X", onde X é
 //o N-ésimo termo da série de Fibonacci.
-
 fun main() {
     println("Digite um número:")
-    val numeroDigitado = readln().toInt()
+    val numDig: Long = readln().toLong()
+    println("Fib($numDig) = ${fibo(numDig)}")
+}
 
-    var vetorFibo = IntArray(numeroDigitado)
-
-    var anterior = 0
-    var proxima = 1
-    var soma = 0
-    val tamanhoFibo = numeroDigitado+1
-
-    for(i in 0..tamanhoFibo){
-        vetorFibo[i] = anterior
-        soma = anterior + proxima
-        anterior = proxima
-        proxima = soma
+fun fibo(num: Long): Long {
+    return if (num == 0L || num == 1L) {
+        num
+    } else {
+        fibo(num - 1) + fibo(num - 2)
     }
-
-    for(i in 0..tamanhoFibo){
-        if(numeroDigitado == 0){
-            println("F(0) = 0")
-        }
-        else if(numeroDigitado == 1){
-            println("F(1) = 1")
-        }
-        else if(numeroDigitado == vetorFibo[numeroDigitado]){
-            println("F($numeroDigitado) = ${vetorFibo[numeroDigitado]}")
-        }
-    }
-//DEU TUDO ERRADO
-
 }
